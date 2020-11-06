c6bb0b243c57efc7cd06caba38129f5985c9351c
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2020-11-06 12:17:16 -0600

    updated BT

--- BT.c
+++ BT.c
@@ -1,19 +1,14 @@
 #include <Arduino.h> //Permite utilizar los comandos de Arduino
-#include <SoftwareSerial.h>//Libreria para nuevo puerto serial para el bluetooth
 #include "BT.h"
 
-#define RX 2
-#define TX 3
 
-extern SoftwareSerial mySerial(RX,TX); // RX, TX
-
-void Bluetooth_inits(void)
+void BT::Bluetooth_inits(void)
 {
 
     mySerial.begin(9600);//Iniciar la comunicacion a 9600
 }
 
-char Bluetooth_read(void)
+char BT::Bluetooth_read(void)
 {
   char Data;
   if (mySerial.available() > 0)
@@ -23,7 +18,7 @@ char Bluetooth_read(void)
   return Data;
 }
 
-void Bluetooth_sent(char Data)
+void BT::Bluetooth_sent(char Data)
 {
   if (mySerial.available() > 0)
       {
--- BT.h
+++ BT.h
@@ -2,11 +2,20 @@
 #ifndef BT_H
 #define BT_H
 
+#include <SoftwareSerial.h>//Libreria para nuevo puerto serial para el bluetooth
+#include "Base_Robot.h"
+class BT
+{
+  private:
+  SoftwareSerial Bluetooth; // RX, TX
 
+  public:
+  Robot_BT(int TX, int RX): Bluetooth(TX,RX){}
+  void Inicializar();
+  char Leer_BT();
 
-void Bluetooth_inits(void);
-char Bluetooth_read(void);
-void Bluetooth_sent(char Data);
+};
+
 
 
 
