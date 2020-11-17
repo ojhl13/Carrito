ea3c7e34bb651bb1352a0a97f9659d2d0618292f
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2020-11-16 22:07:12 -0600

    adding BT function to STOP CAR

--- blackbox.cpp
+++ blackbox.cpp
@@ -8,7 +8,7 @@
 #define MOTOR2 1
 #define TIME 100 //microseconds
 #define MIDSPEED 50
-
+#define BT_STOPSIGNAL 'w'
 
 unsigned char testspeed = 10;
 
@@ -32,6 +32,7 @@ void Start_system(void)
 {
    Start_motors();
    Start_data_convertion();
+   Bluetooth_inits();
 }
 
 float read_speed(void)
@@ -45,8 +46,73 @@ float read_speed(void)
 void response(float out_data)
 {
   int Speed;
+  char BT_datareaded;
   Speed =1;
-  /*convert the output of PID to data to PWM*/
-  Motor_Forward(Speed,MOTOR1);
-  Motor_Forward(Speed,MOTOR2);
+  BT_datareaded = Bluetooth_read();
+  if(BT_STOPSIGNAL == BT_datareaded)
+  {
+    Motor_Stop(MOTOR1);
+    Motor_Stop(MOTOR2);
+  }
+  else
+  {
+    /*convert the output of PID to data to PWM*/
+    Motor_Forward(Speed,MOTOR1);
+    Motor_Forward(Speed,MOTOR2);
+  }
+  delay(100);
 }
+
+/*
+void BT_CARCULATOR_INSTRUCTIONS_2MOVE(char data)
+{
+  if (data == '1')
+      {
+        //Arriba_Izquierda
+
+      }
+      if (data == '2')
+      {
+        //Derecho
+
+      }
+      if (data == '3')
+      {
+        //Arriba_Derecha
+
+      }
+      if (data == '4')
+      {
+        //Girar a la izquierda
+      }
+      if (data == '5')
+      {
+        //Serial.println("Logo talos");
+      }
+      if (data == '6')
+      {
+        //Girar a la derecha
+
+
+      }
+      if (data == '7')
+      {
+        //Abajo Izquierda
+
+      }
+      if (data == '8')
+      {
+        //Reversa
+
+      }
+      if (data == '9')
+      {
+        //Abajo Derecha
+
+      }
+      if (data == 'w')
+      {
+        //STOP
+      }
+}
+*/
--- blackbox.h
+++ blackbox.h
@@ -1,11 +1,8 @@
 #ifndef BLACKBOX_H
 #define BLACKBOX_H
-
-
-
   void Start_system(void);
   float test (void);
   float read_speed(void);
   void response(float out_data);
-  
+
 #endif
